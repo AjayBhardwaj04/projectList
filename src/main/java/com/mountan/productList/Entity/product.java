b66bda0251c5ece46cfecb747ffffb3aa34d747c
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-30 19:36:49 -0700

    Entity produc code

--- src/main/java/com/mountan/productList/Entity/product.java
+++ src/main/java/com/mountan/productList/Entity/product.java
@@ -1,4 +1,22 @@
 package com.mountan.productList.Entity;
 
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private category category;
 }
