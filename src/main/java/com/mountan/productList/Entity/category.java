fb0b37ea5d5e90942a2fd82c3fb81841873bdae2
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-30 19:35:41 -0700

    Entity product category code

--- src/main/java/com/mountan/productList/Entity/category.java
+++ src/main/java/com/mountan/productList/Entity/category.java
@@ -1,4 +1,21 @@
 package com.mountan.productList.Entity;
 
-public class categre {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.repository.cdi.Eager;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<product> products;
 }
