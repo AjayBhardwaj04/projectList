5e1599f1b60857785b776f4982074703bfa0ce46
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-30 19:34:30 -0700

    code

--- src/main/java/com/mountan/productList/Controller/productController.java
+++ src/main/java/com/mountan/productList/Controller/productController.java
@@ -1,4 +1,10 @@
 package com.mountan.productList.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
 public class productController {
+
 }
