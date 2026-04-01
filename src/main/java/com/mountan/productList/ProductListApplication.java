dd6a581f80a29fb6066bd1bf653c681936998b64
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-03-31 18:25:22 -0700

    main runing Application

--- src/main/java/com/mountan/productList/ProductListApplication.java
+++ src/main/java/com/mountan/productList/ProductListApplication.java
@@ -2,12 +2,16 @@ package com.mountan.productList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProductListApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductListApplication.class, args);
+		ApplicationContext context = SpringApplication.run(ProductListApplication.class, args);
+
+
+
 	}
 
 }
