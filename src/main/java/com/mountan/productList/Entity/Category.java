af132b9a95a9118452033865aab6733321219695
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-01 19:49:50 -0700

    rename file name

--- src/main/java/com/mountan/productList/Entity/Category.java
+++ src/main/java/com/mountan/productList/Entity/Category.java
@@ -3,19 +3,18 @@ package com.mountan.productList.Entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.repository.cdi.Eager;
 
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-public class category {
+public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
-    private List<product> products;
+    private List<Product> products;
 }
