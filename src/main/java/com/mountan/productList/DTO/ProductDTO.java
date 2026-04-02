a03f8a142e3b13565b3ed55bf6ce5a0ea5f9118b
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-01 19:49:30 -0700

    rename file name

--- src/main/java/com/mountan/productList/DTO/ProductDTO.java
+++ src/main/java/com/mountan/productList/DTO/ProductDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class productDTO {
+public class ProductDTO {
     private long id;
     private String name;
     private String description;
