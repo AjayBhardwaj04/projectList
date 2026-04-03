e04ee41bc917d026447f25265f59122f81b72174
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-02 19:45:16 -0700

    categoryMapper fun. code

--- src/main/java/com/mountan/productList/Mapper/CategoryMapper.java
+++ src/main/java/com/mountan/productList/Mapper/CategoryMapper.java
@@ -4,6 +4,18 @@ import com.mountan.productList.DTO.CategoryDTO;
 import com.mountan.productList.Entity.Category;
 
 public class CategoryMapper {
+
+    public static CategoryDTO toCategoryDTO(Category category) {
+        if (category == null) {
+            return null;
+        }
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setId(category.getId());
+        categoryDTO.setName(category.getName());
+        categoryDTO.getProducts(category.getProducts().stream().map(ProductMapper::toProductDTO).toList());
+        return categoryDTO;
+    }
+
     public static Category toCategoryEntity(CategoryDTO categoryDTO){
         Category category = new Category(); // "Category " this Category import from Entity Category
         category.setName(categoryDTO.getName());
