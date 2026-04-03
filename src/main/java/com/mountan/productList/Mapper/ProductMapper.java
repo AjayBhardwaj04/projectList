e5ee9ca495255fda08421dd0e31aa1cec13e7324
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-02 19:45:38 -0700

    categoryMapper fun. code

--- src/main/java/com/mountan/productList/Mapper/ProductMapper.java
+++ src/main/java/com/mountan/productList/Mapper/ProductMapper.java
@@ -1,5 +1,31 @@
 package com.mountan.productList.Mapper;
 
+import com.mountan.productList.DTO.ProductDTO;
+import com.mountan.productList.Entity.Category;
+import com.mountan.productList.Entity.Product;
+
 public class ProductMapper {
+    //entity to dto
+    public static ProductDTO toProductDTO(Product product) {
+        return new ProductDTO(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getCategory().getId()
+        );
+
+
+    }
+    //        DTO to Entity
+    public static Product toProduct(ProductDTO productDTO, Category  category) {
+        Product product = new Product();
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        product.setCategory(category);
+
+        return product;
+    }
     
 }
