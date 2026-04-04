e9ec9feb4c204085fa8312b2dd2f3ebef9853aed
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-03 20:11:12 -0700

    categoryMapper fun. code

--- src/main/java/com/mountan/productList/Controller/CategoryController.java
+++ src/main/java/com/mountan/productList/Controller/CategoryController.java
@@ -1,10 +1,25 @@
 package com.mountan.productList.Controller;
 
+import com.mountan.productList.DTO.CategoryDTO;
+import com.mountan.productList.Service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/categories")
+@RequestMapping("/api/category")
+@AllArgsConstructor
 public class CategoryController {
+
+    private  CategoryService categoryService;
+
+
+
+    @PostMapping
+    public CategoryDTO createCategory(@RequestBody CategoryDTO categoryDTO){
+        return categoryService.toCategoryDTO(categoryDTO);
+    }
 
 }
