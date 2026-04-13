4d9179e158884816f1fe17e1762566c55c99636f
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-13 08:01:14 +0530

    code update

--- src/main/java/com/mountan/productList/ProductListApplication.java
+++ src/main/java/com/mountan/productList/ProductListApplication.java
@@ -3,15 +3,14 @@ package com.mountan.productList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+// import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProductListApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(ProductListApplication.class, args);
-
-
-
+//		SpringApplication.run(ProductListApplication.class, args);
+		 ApplicationContext context = SpringApplication.run(ProductListApplication.class, args);
 	}
 
 }
