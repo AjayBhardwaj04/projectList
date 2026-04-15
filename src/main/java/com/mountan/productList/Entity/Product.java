9652f3d204d827b36b42c878bd2a3fb1cee653de
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-15 07:29:53 +0530

    code update

--- src/main/java/com/mountan/productList/Entity/Product.java
+++ src/main/java/com/mountan/productList/Entity/Product.java
@@ -17,6 +17,6 @@ public class Product {
     private double price;
 
     @ManyToOne
-    @JoinColumn(name = "category_id")
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 }
