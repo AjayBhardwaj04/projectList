be8696ac049eb1f56ed74e251f5e1e869989b004
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-15 07:35:56 +0530

    create category bussnis logic to catrgoryDTO

--- src/main/java/com/mountan/productList/Service/CategoryService.java
+++ src/main/java/com/mountan/productList/Service/CategoryService.java
@@ -5,18 +5,25 @@ import com.mountan.productList.DTO.CategoryDTO;
 import com.mountan.productList.Entity.Category;
 import com.mountan.productList.Mapper.CategoryMapper;
 import com.mountan.productList.Repository.CategoryRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CategoryService {
-    public CategoryRepository categoryRepository;
-    public CategoryDTO toCategoryDTO(CategoryDTO categoryDTO){
+    private final CategoryRepository categoryRepository;
+
+
+
+    public CategoryDTO CreateCategory(CategoryDTO categoryDTO){
        Category category = CategoryMapper.toCategoryEntity(categoryDTO);
        category = categoryRepository.save(category);
       return CategoryMapper.toCategoryDTO(category);
 
 
+
     }
+
 
 
 }
