b311a8328e25c36b0185dbf7064af30895b9399e
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-16 22:53:07 +0530

    Product service logic code

--- src/main/java/com/mountan/productList/DTO/ProductDTO.java
+++ src/main/java/com/mountan/productList/DTO/ProductDTO.java
@@ -8,10 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDTO {
-    private long id;
+
     private String name;
     private String description;
     private double price;
     private  Long CategoryID;
-    
+
 }
