6752e199cc7d89891032722afc3ea111ca493bd0
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-16 22:54:22 +0530

    product mapper code

--- src/main/java/com/mountan/productList/Mapper/ProductMapper.java
+++ src/main/java/com/mountan/productList/Mapper/ProductMapper.java
@@ -7,18 +7,18 @@ import com.mountan.productList.Entity.Product;
 public class ProductMapper {
     //entity to dto
     public static ProductDTO toProductDTO(Product product) {
-        return new ProductDTO(
-                product.getId(),
-                product.getName(),
-                product.getDescription(),
-                product.getPrice(),
-                product.getCategory().getId()
-        );
+     return  new ProductDTO(
+             product.getName(),
+             product.getDescription(),
+             product.getPrice(),
+             product.getCategory().getId()
+
+     );
 
 
     }
     //        DTO to Entity
-    public static Product toProduct(ProductDTO productDTO, Category  category) {
+    public static Product toProductEntity(ProductDTO productDTO, Category  category) {
         Product product = new Product();
         product.setName(productDTO.getName());
         product.setDescription(productDTO.getDescription());
