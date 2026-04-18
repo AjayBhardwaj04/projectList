6ea62a71d02ea87e1d05d09fa8d136df23aea8a2
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-18 07:52:46 +0530

    create get , post ,delete mapping

--- src/main/java/com/mountan/productList/Controller/ProductController.java
+++ src/main/java/com/mountan/productList/Controller/ProductController.java
@@ -3,24 +3,48 @@ package com.mountan.productList.Controller;
 import com.mountan.productList.DTO.ProductDTO;
 import com.mountan.productList.Service.ProductService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@AllArgsConstructor
-//@NoArgsConstructor
 @RequestMapping("/api/product")
+@AllArgsConstructor
 public class ProductController {
     private ProductService productService;
 
+    // GET ALL PRODUCTS
+    @GetMapping
+    public List<ProductDTO> getProduct(){
+        return productService.getAllProduct();
+    }
+
+    //GET PRODUCT BY ID
+    @GetMapping("/{id}")
+    public ProductDTO getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
+
+
+    // CREATE PRODUCT MAPPING
 @PostMapping
-    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
-        return new ResponseEntity<>( productService.createProduct(productDTO), HttpStatus.CREATED);
+    public ResponseEntity<ProductDTO> createProducts(@RequestBody ProductDTO productDTO){
+       ProductDTO createProduct = productService.createProduct(productDTO);
+       return new ResponseEntity<>(createProduct,HttpStatus.CREATED);
+    }
+// UPDATE PRODUCTS
+    @PutMapping("/{id}")
+    public ProductDTO updateProducts(@PathVariable Long id ,@RequestBody ProductDTO productDTO){
+       return productService.updateProduct(id,productDTO);
+
+    }
+    //DELETE PRODUCTS
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable Long id){
+
+        return productService.deleteProduct(id);
     }
 
 }
