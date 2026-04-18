264dcb32f2441f60c73c2c2b2f8075abc9852efe
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-18 07:52:01 +0530

    create get , post ,delete mapping

--- src/main/java/com/mountan/productList/Controller/CategoryController.java
+++ src/main/java/com/mountan/productList/Controller/CategoryController.java
@@ -1,22 +1,29 @@
 package com.mountan.productList.Controller;
 
 import com.mountan.productList.DTO.CategoryDTO;
+import com.mountan.productList.DTO.ProductDTO;
 import com.mountan.productList.Service.CategoryService;
+import com.mountan.productList.Service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
 @AllArgsConstructor
 public class CategoryController {
-
+    private ProductService productService;
     private  CategoryService categoryService;
+// get all category
+    @GetMapping
+    public List<CategoryDTO> getAllCategory(){
+        return   categoryService.getAllCategory();
+    }
+
 
 
 
@@ -24,5 +31,19 @@ public class CategoryController {
     public ResponseEntity< CategoryDTO> createCategory( @Validated @RequestBody CategoryDTO categoryDTO){
         return new ResponseEntity<>( categoryService.CreateCategory(categoryDTO), HttpStatus.CREATED);
     }
+
+    // get category by id
+    @GetMapping("/{id}")
+    public CategoryDTO getByIdCategory(@PathVariable Long id){
+        return categoryService.getByIdCategory(id);
+    }
+
+// DELETE category by id
+    @DeleteMapping("/{id}")
+public String categoryDeleteById(@PathVariable Long id){
+     return categoryService.categoryDeleteById(id);
+
+    }
+
 
 }
