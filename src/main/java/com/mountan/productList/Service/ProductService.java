5758f565eca3af04509f48a3546284ddb40b8e89
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-19 07:54:54 +0530

    update Product service  code

--- src/main/java/com/mountan/productList/Service/ProductService.java
+++ src/main/java/com/mountan/productList/Service/ProductService.java
@@ -8,6 +8,9 @@ import com.mountan.productList.Repository.CategoryRepository;
 import com.mountan.productList.Repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -29,5 +32,40 @@ public class ProductService {
          //entity  to dto
          return ProductMapper.toProductDTO(product);
     }
+
+    //GET ALL PRODUCTS
+    public List<ProductDTO> getAllProduct ( ){
+      return productRepository.findAll().stream().map(ProductMapper ::toProductDTO).toList();
+    }
+
+    //GET PRODUCT BY IDs
+    public ProductDTO getProductById( Long id){
+       Product product = productRepository.findById(id).orElseThrow(
+               () -> new RuntimeException("PRODUCT ID NOT FOUNDS"));
+         return ProductMapper.toProductDTO(product);
+    }
+
+    // Update Products
+    public ProductDTO updateProduct( Long id, ProductDTO productDTO){
+       Product product = productRepository.findById(id).orElseThrow(
+               ()-> new RuntimeException("PRODUCT NOT FIND"));
+       Category category = categoryRepository.findById(productDTO.getCategoryID()).orElseThrow(
+               ()-> new RuntimeException("CATEGORY NOT FOUND"));
+
+       product.setName(productDTO.getName());
+       product.setDescription(productDTO.getDescription());
+       product.setPrice(productDTO.getPrice());
+       product.setCategory(category);
+       productRepository.save(product);
+       return  ProductMapper.toProductDTO(product);
+    }
+
+    //DELETE PRODUCT
+    public  String deleteProduct( Long id){
+        productRepository.deleteById(id);
+        return "Product ID "+ id + " has been DELETED";
+    }
+
+
 
 }
