06400036e7101cc6bbae303b21809fc8e5b6dda1
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-19 07:54:18 +0530

    update category service  code

--- src/main/java/com/mountan/productList/Service/CategoryService.java
+++ src/main/java/com/mountan/productList/Service/CategoryService.java
@@ -8,21 +8,40 @@ import com.mountan.productList.Repository.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
 
+    public CategoryDTO CreateCategory(CategoryDTO categoryDTO) {
+        Category category = CategoryMapper.toCategoryEntity(categoryDTO);
+        category = categoryRepository.save(category);
+        return CategoryMapper.toCategoryDTO(category);
+    }
+// get all category
+    public List<CategoryDTO> getAllCategory(){
+       return categoryRepository.findAll().stream().map(CategoryMapper ::toCategoryDTO).toList();
+    }
+// get category by id
 
-    public CategoryDTO CreateCategory(CategoryDTO categoryDTO){
-       Category category = CategoryMapper.toCategoryEntity(categoryDTO);
-       category = categoryRepository.save(category);
-      return CategoryMapper.toCategoryDTO(category);
+public CategoryDTO getByIdCategory(Long id){
+        Category category = categoryRepository.findById(id).orElseThrow(()-> new RuntimeException("CATEGORY NOR FOUND"));
+         return CategoryMapper.toCategoryDTO(category);
+//        categoryRepository.findById(id).orElseThrow(()-> new RuntimeException("CATEGORY NOT FOUND "));
+}
+
+// delete category by id
+    public String  categoryDeleteById(Long id){
+        categoryRepository.deleteById(id);
+        return " CATEGORY " + id +" has ben deletes";
 
 
 
     }
+
 
 
 
