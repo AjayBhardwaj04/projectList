16a4d7c143db67be52609e27d97a1e2ce10e3377
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-25 08:32:19 +0530

    Category Exception handler code

--- src/main/java/com/mountan/productList/Exception/categoryException.java
+++ src/main/java/com/mountan/productList/Exception/categoryException.java
@@ -1,4 +1,13 @@
 package com.mountan.productList.Exception;
 
-public class catrgoryException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class categoryException extends RuntimeException{
+    public categoryException(String message) {
+
+        super(message);
+    }
+
 }
