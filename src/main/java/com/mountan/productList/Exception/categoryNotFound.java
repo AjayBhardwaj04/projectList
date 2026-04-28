43dee46723638a32cc63b919b60a6a065006f446
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-04-28 08:26:02 +0530

    Exception hendler for category list

---
+++ src/main/java/com/mountan/productList/Exception/categoryNotFound.java
@@ -0,0 +1,12 @@
+package com.mountan.productList.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class categoryNotFound extends RuntimeException {
+    public categoryNotFound(String message) {
+        super(message);
+    }
+
+}
