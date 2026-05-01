9ab115a9641405cda04f3c92a5d28aabe4dd8c00
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-05-01 07:25:20 +0530

    Global exception hendler code update for category:

--- src/main/java/com/mountan/productList/DTO/exceptionResponseDTO.java
+++ src/main/java/com/mountan/productList/DTO/exceptionResponseDTO.java
@@ -1,4 +1,15 @@
 package com.mountan.productList.DTO;
 
-public class exceptionResponsDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+public class exceptionResponseDTO {
+    private String apiPath;
+    private HttpStatus httpCode;
+    private  String errorMessage;
+    private LocalDateTime errorTime;
 }
