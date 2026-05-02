6de98ca49fb406afecda5e50fce6019eb8bb944e
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-05-02 08:04:53 +0530

    globalExceptionHandlerException

--- src/main/java/com/mountan/productList/Exception/globalExceptionHandlerException.java
+++ src/main/java/com/mountan/productList/Exception/globalExceptionHandlerException.java
@@ -1,19 +1,57 @@
 package com.mountan.productList.Exception;
 
+import com.mountan.productList.DTO.exceptionResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class globalExceptionHandlerException {
-@ExceptionHandler(categoryException.class)
-    public ResponseEntity<String> globalExceptionHandler(categoryException ex){
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+
+
+
+
+    @ExceptionHandler(categoryException.class)
+    public ResponseEntity<exceptionResponseDTO> globalExceptionHandler(categoryException ex, WebRequest webRequest) {
+       exceptionResponseDTO exceptionResponseDTO = new exceptionResponseDTO(
+               webRequest.getDescription(false),
+               HttpStatus.CONFLICT,
+               ex.getMessage(),
+               LocalDateTime.now()
+       );
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionResponseDTO);
     }
 
     @ExceptionHandler(categoryNotFound.class)
-    public ResponseEntity<String> categoryNotFound(categoryNotFound ex){
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    public ResponseEntity<exceptionResponseDTO> categoryNotFound(categoryNotFound ex, WebRequest webRequest) {
+
+        exceptionResponseDTO exceptionResponseDTO = new exceptionResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponseDTO);
     }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<exceptionResponseDTO> handelGlobalException(Exception ex, WebRequest webRequest) {
+
+        exceptionResponseDTO exceptionResponseDTO = new exceptionResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exceptionResponseDTO);
+    }
+
 }
