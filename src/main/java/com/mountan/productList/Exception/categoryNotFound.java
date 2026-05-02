6ee5adc388712fa37322bfb3ad4bf5252392f08f
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-05-02 08:04:08 +0530

    EXCEPTION HENDEL IN CATEGORY

--- src/main/java/com/mountan/productList/Exception/categoryNotFound.java
+++ src/main/java/com/mountan/productList/Exception/categoryNotFound.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
+
 public class categoryNotFound extends RuntimeException {
     public categoryNotFound(String message) {
         super(message);
