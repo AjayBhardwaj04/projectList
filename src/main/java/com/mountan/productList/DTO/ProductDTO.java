7f7dab18dcb09d4cc3d4158449369bf67fb691bf
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-05-03 08:02:10 +0530

    SWAGGER MASSEG INTEGRETION

--- src/main/java/com/mountan/productList/DTO/ProductDTO.java
+++ src/main/java/com/mountan/productList/DTO/ProductDTO.java
@@ -1,9 +1,13 @@
 package com.mountan.productList.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@Schema(
+        name = "Product",
+        description = "It hold product information"
+)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
