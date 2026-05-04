b95a2ea9f3833f01fe23eff660b6eca6b8589f47
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-05-04 07:30:00 +0530

    code update

--- src/main/java/com/mountan/productList/Entity/User.java
+++ src/main/java/com/mountan/productList/Entity/User.java
@@ -1,4 +1,18 @@
 package com.mountan.productList.Entity;
 
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Users")
+//@Table(name = "U?sers")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
 }
