e7939469dbf89957887aecc3f6dc5ec6e4379046
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-05-04 07:29:38 +0530

    code update

--- src/main/java/com/mountan/productList/Controller/userController.java
+++ src/main/java/com/mountan/productList/Controller/userController.java
@@ -1,4 +1,22 @@
 package com.mountan.productList.Controller;
 
+import com.mountan.productList.Entity.User;
+import com.mountan.productList.Service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
 public class userController {
+    @Autowired
+    private MyUserDetailsService service;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user){
+       return service.createUser(user);
+    }
 }
