286bb935e04948aaf25702aa9a0fdd73fa698089
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-05-05 07:44:55 +0530

    code update

--- src/main/java/com/mountan/productList/ProductListApplication.java
+++ src/main/java/com/mountan/productList/ProductListApplication.java
@@ -1,11 +1,26 @@
 package com.mountan.productList;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 // import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Product service REST API documentation ",
+				description = "Product REST API",
+				version = "v1",
+				contact = @Contact(
+						name = "Ajay",
+						email = "ajay@gmail.com"
+				)
+
+		)
+)
 public class ProductListApplication {
 
 	public static void main(String[] args) {
