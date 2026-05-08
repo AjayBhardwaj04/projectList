74e115f9e35d7d99717721b6508ff30c6d4513eb
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-05-08 07:41:45 +0530

    code update

--- src/main/java/com/mountan/productList/Repository/ProductRepository.java
+++ src/main/java/com/mountan/productList/Repository/ProductRepository.java
@@ -4,7 +4,17 @@ import com.mountan.productList.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
-}
+
+        boolean existsByNameAndPriceAndDescriptionAndCategoryId(
+                String name,
+                BigDecimal price,
+                String description,
+                Long categoryId
+        );
+    }
+
