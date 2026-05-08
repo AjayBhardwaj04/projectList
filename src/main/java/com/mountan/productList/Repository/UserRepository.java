65b2071d54cbfe67fda72400ce29c3dd34e72855
Ajay Bhardwaj <bhardwajajay0808@gmail.com>
2026-05-08 07:42:02 +0530

    code update

--- src/main/java/com/mountan/productList/Repository/UserRepository.java
+++ src/main/java/com/mountan/productList/Repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.mountan.productList.Repository;
 
-public interface UserReository {
+import com.mountan.productList.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+
 }
